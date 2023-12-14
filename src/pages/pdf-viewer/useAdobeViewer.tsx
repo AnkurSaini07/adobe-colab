e72dbf4d6faf935dad258973bd678b61d12ac604
ankurs <ankurs@adobe.com>
2023-12-14 20:59:37 +0530

    initial commit

--- src/pages/pdf-viewer/useAdobeViewer.tsx
+++ src/pages/pdf-viewer/useAdobeViewer.tsx
@@ -9,29 +9,21 @@ interface UserAdobeViewerProps {
     isViewerDisabled: boolean;
 }
 
-const xy: any = [];
-let pageNumber = 0;
-
 export default function useAdobeViewer(props: UserAdobeViewerProps) {
     const {divId, localUser, presenterId, isViewerDisabled} = props;
     const [viewer, setViewer] = useState<any>();
-    const [adobeDCView, setAdobeDCView] = useState<any>();
     const [annotationManager, setAnnotationManager] = useState<any>();
     const [viewerState, setViewerState] = useSharedState(`pdf-viewer-state-${divId}`, {
-        pageNumber: undefined,
-        zoomLevel: undefined,
-        x: undefined,
-        y: undefined,
+        currentPage: undefined,
+        zoomLevel: undefined
     });
-
 
     useEffect(() => {
         if (!viewer) {
             //@ts-ignore
-            const _adobeDCView = new AdobeDC.View({clientId: "3e821bd37b2d405c8ebba6327a0998fa", divId});
-            setAdobeDCView(_adobeDCView);
+            const adobeDCView = new AdobeDC.View({clientId: "3e821bd37b2d405c8ebba6327a0998fa", divId});
             //@ts-ignore
-            _adobeDCView.registerCallback(AdobeDC.View.Enum.CallbackType.GET_USER_PROFILE_API, async function () {
+            adobeDCView.registerCallback(AdobeDC.View.Enum.CallbackType.GET_USER_PROFILE_API, async function () {
                 return {
                     // @ts-ignore
                     code: AdobeDC.View.Enum.ApiResponseCode.SUCCESS,
@@ -45,51 +37,51 @@ export default function useAdobeViewer(props: UserAdobeViewerProps) {
                     }
                 };
             }, {});
-            _adobeDCView.previewFile({
+            adobeDCView.previewFile({
                 content: {location: {url: "https://acrobatservices.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf"}},
                 metaData: {fileName: "Bodea Brochure.pdf", id: "77c6fa5d-6d74-4104-8349-657c8411a834"}
             }, {
                 showAnnotationTools: true,
-                enableAnnotationAPIs: true
+                enableAnnotationAPIs: true,
+                enableInternalEvents: true,
+                enableFilePreviewEvents: true,
             }).then(async (_viewer: any) => {
                 setViewer(_viewer);
                 setAnnotationManager(await _viewer.getAnnotationManager());
-            });
 
+                // @ts-ignore
+                window.viewer = _viewer;
+            });
         }
     }, []);
 
     useEffect(() => {
-        if (adobeDCView && viewer) {
-            // @ts-ignore
-            adobeDCView.registerCallback(AdobeDC.View.Enum.CallbackType.EVENT_LISTENER, async function (event) {
-                if (presenterId === localUser.userId) {
-                    if (event.type === "DOCUMENT_VIEW_STATE_UPDATE") {
-                        xy[0] = event.data.x;
-                        xy[1] = event.data.y;
-                        pageNumber = event.data.pageNumber;
-                        const apis = await viewer.getAPIs();
-                        const zoomLevel = await apis.getPageZoom(pageNumber);
-                        //@ts-ignore
-                        setViewerState({pageNumber, zoomLevel, x: xy[0], y: xy[1]});
+        let intervalId: any;
+        if (viewer && presenterId === localUser.userId) {
+            intervalId = setInterval(async () => {
+                const apis = await viewer.getAPIs();
+                const currentPage = await apis.getCurrentPage();
+                const zoomLevel = await apis.getPageZoom(currentPage);
+                setViewerState({currentPage, zoomLevel});
+            }, 1000);
 
-                    }
-                }
-            }, {
-                enableInternalEvents: true,
-                enableFilePreviewEvents: true,
-            });
         }
-    }, [adobeDCView, presenterId, localUser, setViewerState, viewer]);
+
+        return () => {
+            if (intervalId) {
+                return clearInterval(intervalId);
+            }
+        }
+
+    }, [viewer, presenterId, localUser, setViewerState]);
 
 
     // isViewerDisabled means its in presentation mode
     useEffect(() => {
         if (viewer && isViewerDisabled && presenterId !== localUser.userId) {
             viewer.getAPIs().then(async (apis: any) => {
-                if (Number.isInteger(viewerState.pageNumber)) {
-                    //@ts-ignore
-                    apis.gotoLocation(viewerState.pageNumber, viewerState.x, viewerState.y);
+                if (Number.isInteger(viewerState.currentPage)) {
+                    apis.gotoLocation(viewerState.currentPage);
                     apis.getZoomAPIs().setZoomLevel(viewerState.zoomLevel);
                 }
             });
